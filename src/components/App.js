738a3b62c06cbbe560f8c0ab50305fc437045303
sanjeetsk <ssanjeetkumar67@gmail.com>
2023-09-04 18:05:20 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,44 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, {useState,useEffect} from "react"
 
 const App = () => {
+
+  const fruits= ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+  const [searchTerm, setSearchTerm] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const filteredSuggestions = fruits.filter(fruit => fruit.includes(searchTerm.toLowerCase()));
+      setSuggestions(filteredSuggestions);
+    });
+
+    return ()=> clearTimeout(timer);
+    
+  }, [searchTerm]);
+
+  const handleSuggestionClick = (suggestion) => {
+    setSearchTerm(suggestion);
+    setSuggestions([]);
+  }
+  
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div id="main">
+      <h1>Search item</h1>
+        <input
+          type="text"
+          placeholder="Search for fruits..."
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)}
+        />
+        <ul>
+          {
+            suggestions.map((suggestion, index) =>(
+              <li key={index} onClick={() => handleSuggestionClick(suggestion)} >
+                {suggestion}
+              </li>
+            ))
+          }
+        </ul>
     </div>
   )
 }
